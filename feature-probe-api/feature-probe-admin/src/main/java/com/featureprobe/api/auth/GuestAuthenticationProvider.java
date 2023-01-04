d3320e5419e9da0291f730d95456baa311d03a3b
denger <dengfeige@featureprobe.com>
2023-01-04 12:13:38 +0800

    fix: API should not be selected as a Approver(#50) (#88)
    * fix: API should not be selected as a Approver(#50)
    
    * fix: query members by source
    
    Co-authored-by: dengfeige <dengfeige@featureprobe.io>

--- feature-probe-api/feature-probe-admin/src/main/java/com/featureprobe/api/auth/GuestAuthenticationProvider.java
+++ feature-probe-api/feature-probe-admin/src/main/java/com/featureprobe/api/auth/GuestAuthenticationProvider.java
@@ -1,12 +1,14 @@
 package com.featureprobe.api.auth;
 
+import com.featureprobe.api.base.enums.MemberSourceEnum;
+import com.featureprobe.api.base.enums.OperationType;
 import com.featureprobe.api.dao.entity.Member;
 import com.featureprobe.api.dao.entity.OperationLog;
-import com.featureprobe.api.base.enums.OperationType;
 import com.featureprobe.api.service.GuestService;
 import com.featureprobe.api.service.MemberService;
 import com.featureprobe.api.service.OperationLogService;
 import lombok.AllArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -33,17 +35,25 @@ public class GuestAuthenticationProvider implements AuthenticationProvider {
         OperationLog log = new OperationLog(OperationType.LOGIN.name() + "_" + token.getSource(),
                 token.getAccount());
         if (member.isPresent()) {
+            if (isAccessTokenMumber(member)) {
+                return null;
+            }
             memberService.updateVisitedTime(token.getAccount());
             operationLogService.save(log);
             return new UserPasswordAuthenticationToken(AuthenticatedMember.create(member.get()),
                     Arrays.asList());
-        } else {
-            Member newMember = guestService.initGuest(token.getAccount(), token.getSource());
-            operationLogService.save(log);
-            return new UserPasswordAuthenticationToken(AuthenticatedMember.create(newMember),
-                    Arrays.asList());
         }
+
+        Member newMember = guestService.initGuest(token.getAccount(), token.getSource());
+        operationLogService.save(log);
+        return new UserPasswordAuthenticationToken(AuthenticatedMember.create(newMember),
+                Arrays.asList());
     }
+
+    private boolean isAccessTokenMumber(Optional<Member> member) {
+        return StringUtils.equalsIgnoreCase(member.get().getSource(), MemberSourceEnum.ACCESS_TOKEN.name());
+    }
+
 
     @Override
     public boolean supports(Class<?> authentication) {
