f778a7a09baa925f909b45d894502e4d67bccad1
panchongyang <yourhand1025@163.com>
2023-02-17 10:11:06 +0800

    chore: SizeTips unit test

---
+++ ui/src/components/SizeTips/index.test.tsx
@@ -0,0 +1,49 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import userEvent from '@testing-library/user-event';
+import SizeTips from '.';
+import { IntlWrapper } from 'components/utils/wrapper';
+
+it('SizeTips snapshot', (done) => {
+  (async () => {
+    const { asFragment, rerender } = render(
+      <SizeTips
+        hide={false}
+        onConfirm={function (): void {
+          throw new Error('Function not implemented.');
+        }}
+        size={0}
+      />,
+      {
+        wrapper: IntlWrapper,
+      }
+    );
+    expect(asFragment()).toMatchSnapshot();
+
+    rerender(
+      <SizeTips
+        hide={true}
+        onConfirm={function (): void {
+          throw new Error('Function not implemented.');
+        }}
+        size={0}
+      />
+    );
+    expect(asFragment()).toMatchSnapshot();
+    done();
+  })();
+});
+
+test('click SizeTips', (done) => {
+  (async () => {
+    const handleConfirm = jest.fn();
+    render(<SizeTips onConfirm={handleConfirm} size={0} hide={false}></SizeTips>, {
+      wrapper: IntlWrapper,
+    });
+
+    await userEvent.click(screen.getByText('Got it'));
+    expect(handleConfirm).toBeCalled();
+
+    done();
+  })();
+});
