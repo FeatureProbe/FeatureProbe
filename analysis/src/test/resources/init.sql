d7767b33b49339aa076926e165d72c5c24bc8cda
SSebo <zhangshibo@featureprobe.com>
2023-02-23 18:17:19 +0800

    fix(analysis): value can be null

--- analysis/src/test/resources/init.sql
+++ analysis/src/test/resources/init.sql
@@ -14,7 +14,7 @@ create table events
     time     bigint           not null,
     user_key varchar(256)     not null,
     name     varchar(256)     not null,
-    value    double precision not null,
+    value    double precision null,
     sdk_key  varchar(256)     not null
 );
 
