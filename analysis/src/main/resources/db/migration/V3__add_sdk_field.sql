5c93116cebd4dfe14efd714a35a08142c36d718a
dengfeige <dengfeige@featureprobe.io>
2023-03-22 19:32:52 +0800

    fix: alert table events sql error

--- analysis/src/main/resources/db/migration/V3__add_sdk_field.sql
+++ analysis/src/main/resources/db/migration/V3__add_sdk_field.sql
@@ -1,2 +1,2 @@
-alter table events add sdk_type VARCHAR(64) default '' not null after sdk_key;
-alter table events add sdk_version VARCHAR(64) default '' not null after sdk_key;
+alter table events add sdk_type VARCHAR(64) default '' not null;
+alter table events add sdk_version VARCHAR(64) default '' not null;
