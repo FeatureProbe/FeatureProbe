b0660225c12239b62c288b07e11ba7f3cd69a6ca
jianggang <gangb.info@gmail.com>
2023-05-26 15:16:58 +0800

    :sparkles: chore: remove flyway 36 database name

--- api/admin/src/main/resources/db/migration/V36__add_change_log_table.sql
+++ api/admin/src/main/resources/db/migration/V36__add_change_log_table.sql
@@ -8,7 +8,7 @@ create table publish_message
 )ENGINE=InnoDB collate = utf8mb4_unicode_ci;
 alter table environment
     add version bigint default 1 not null after `deleted`;
-INSERT INTO feature_probe.dictionary (value, `key`, account, deleted, modified_time, created_by, created_time,
+INSERT INTO dictionary (value, `key`, account, deleted, modified_time, created_by, created_time,
                                       modified_by)
 VALUES ('1', 'all_sdk_key_map', '', 0, '2022-10-18 14:50:36', -1, '2022-10-18 14:50:44', -1);
 
