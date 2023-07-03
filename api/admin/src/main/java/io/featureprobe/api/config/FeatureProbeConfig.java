4538717d1ffa5dbda871e74223809a254099a4cc
jianggang <gangb.info@gmail.com>
2023-07-03 17:43:36 +0800

    :sparkles: feat: Extracting the SDK key from the self-reliant SDK

--- api/admin/src/main/java/io/featureprobe/api/config/FeatureProbeConfig.java
+++ api/admin/src/main/java/io/featureprobe/api/config/FeatureProbeConfig.java
@@ -3,6 +3,8 @@ package io.featureprobe.api.config;
 import com.featureprobe.sdk.server.FPConfig;
 import com.featureprobe.sdk.server.FeatureProbe;
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,14 +15,16 @@ import java.net.URL;
 import java.time.Duration;
 
 @Configuration
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class FeatureProbeConfig {
 
-    private ServerProperties serverProperties;
+    private final ServerProperties serverProperties;
     private static final String LOCAL_HOST = "http://127.0.0.1";
     private static final String FEATURE_PROBE_API_EVENT_PATH = "/internal/server/events";
     private static final String FEATURE_PROBE_API_SYNCHRONIZER_PATH = "/internal/server/toggles";
-    private static final String MANAGER_PROJECT_SDK_KEY = "server-t6h78815ef044428826787e9a238b9c6a479f998";
+
+    @Value("${app.featureprobe.sdk-key:server-t6h78815ef044428826787e9a238b9c6a479f998}")
+    private String MANAGER_PROJECT_SDK_KEY;
 
     @Bean
     @Lazy
