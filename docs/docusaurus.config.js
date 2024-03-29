9a340918d4c7379684fec999e068e176958353e1
frankzhaopeng <poryzhao@featureprobe.com>
2024-03-29 17:29:43 +0800

    docs: build on github pages

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -10,8 +10,8 @@ const katex = require('rehype-katex');
 const config = {
   title: 'FeatureProbe',
   tagline: 'An open source feature management service',
-  url: 'https://docs.featureprobe.io',
-  baseUrl: '/',
+  url: 'https://featureprobe.github.io/',
+  baseUrl: '/FeatureProbe/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -21,7 +21,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'FeatureProbe', // Usually your GitHub org/user name.
-  projectName: 'docs', // Usually your repo name.
+  projectName: 'FeatureProbe', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
